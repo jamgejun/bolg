5deaf1201cd945eeee2414a87ff1b1cf885f8163
GrayJay <a1186394662@qq.com>
2019-03-25 21:41:34 +0800

    3.25 change fileName

--- Prototype/list.js
+++ Prototype/list.js
@@ -12,5 +12,34 @@ function fn() {
 
 var x = fn(),
     b = 200
-
 x()
+
+// 函数作为返回值
+function Foo() {
+    var x = 10
+    return function Max(b) {
+        if(x>b) {
+            console.log(x)
+        } else {
+            console.log(b)
+        }
+    }
+}
+
+var f1 = Foo()
+f1(2) // 10
+
+// 作为参数传递
+var max = 10,
+    fn2 = function (x) {
+        if(x>max) {
+            console.log(x)
+        }
+    }
+
+function Max(f){
+    var max = 100
+    f(15)
+}
+
+Max(fn2) // 15
