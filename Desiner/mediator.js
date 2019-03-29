a32e88d923cd2bf028d3f211e972c32a70719c4d
GrayJay <a1186394662@qq.com>
2019-03-29 20:00:59 +0800

    3.29 GrayJay 发布订阅和中介者模式

---
+++ Desiner/mediator.js
@@ -0,0 +1,223 @@
+// 中介者的简单实现
+var mediator = (function (){
+    var topics = {}
+
+    var subscribe = function (topic, fn){
+        if (!topics[topic]) {
+            topics[topic] = []
+        }
+
+        topics[topic].push({
+            context: this, // 关键代码！将context保存为mediator
+            callback: fn
+        })
+
+        return this
+    }
+
+    var publish = function (topic) {
+        var args;
+        if(!topics[topic]) {
+            return false
+        }
+        args = Array.prototype.slice.call(arguments, 1);
+        for(var i = 0; i < topics[topic].length; i++) {
+            var subscription = topics[topic][i]
+            // 将保存在的订阅者信息，将context中的mediator交出来，将this指向mediator
+            subscription.callback.apply(subscription.context, args)
+        }
+        return this
+    }
+
+    return {
+        Publish: publish,
+        Subscribe: subscribe,
+        installTo: function (obj) {
+            obj.subscribe = subscribe;
+            obj.publish = publish;
+        }
+    }
+})()
+
+var GrayJay = mediator.Subscribe('topic', function (){
+    this.name = "GrayJay"
+    console.log(this) // mediator obj
+    console.log('hello, GrayJay')
+})
+
+var Miny = mediator.Subscribe('topic', function (){
+    console.log(this) // mediator obj
+    console.log('hello, Miny')
+})
+
+
+GrayJay.Publish('topic')
+ 
+
+// 中介者的高级实现
+(function (root){
+    function guidGenerator() {}
+
+    // 订阅者构造函数
+    function Subscriber(fn, options, context) {
+        if(!this instanceof Subscriber) {
+            return new Subscriber(fn, options, context)
+        } else {
+            // guidGenerator()是一个函数，用于为订阅者生成GUID，以后很方便的引用他们，
+            this.id = guidGenerator()
+            this.fn = fn
+            this.options = options
+            this.context = context
+            this.topic = null
+        }
+    }
+})()
+
+
+// 
+function Topic( namespace ) {
+    if(!this instanceof Topic) {
+        return new Topic(namespce)
+    } else {
+        this.namespace = namespace || ""
+        this._callbacks = []
+        this._topcis = []
+        this.stopped = false
+    }
+}
+
+Topic.prototype = {
+    AddSubscriber: function (fn, options, context) {
+        // 通过订阅者构造函数，创建一个订阅者
+        var callback = new Subscriber(fn, options, context)
+
+        // 存入订阅者信息
+        this._callbacks.push(callback)
+
+        // 新增订阅者共享了mediator实例
+        callback.topics = this
+        // 返回新增的订阅者
+        return callback
+    },
+    StopPropagation: function () {
+        this.stopped = true
+    },
+
+    // 通过给定的identifier，可以是唯一标识符，可以是topic，也可使给定函数。返回订阅者
+    GetSubscriber: function (identifier) {
+        for(var x = 0, y = this._callbacks.length; x < y; x++) {
+            if(this._callbacks[x].id == identifier || this._callbacks[x].fn == identifier) {
+                return this._callbacks
+            }
+        }
+
+        // 
+        for(var z in this._topcis) {
+            if(this._topcis.hasOwnProperty(z)) {
+                var sub = this._topcis[z].GetSubscriber(identifier)
+                if(sub !== undefined) {
+                    return sub
+                }
+            }
+        }
+    },
+    // 新增topic
+    AddTopic: function (topic) {
+        this._topcis[topic] = new Topic((this.namespace? this.namespace +":":'')+topic)
+    },
+    hasTopic: function (topic) {
+        return this._topcis.hasOwnProperty(topic)
+    },
+    ReturnTopic: function (topic) {
+        return this._topcis[topic]
+    },
+
+    // 移除订阅者列表，通过传入的标识符
+    RemoveSubscriber: function (identifier) {
+        if(!identifier) {
+            this._callbacks = []
+            for(var z in this._topcis) {
+                if(this._topcis.hasOwnProperty(z)) {
+                    this._topcis[z].RemoveSubscriber(identifier)
+                }
+            }
+        }
+
+        for(var y = 0, x = this._callbacks.length; y<x; y++) {
+            if(this._callbacks[y].fn == identifier|| this._callbacks[y].id == identifier) {
+                this._callbacks[y].topic = null
+                this._callbacks.splice(y, 1)
+                x--,y--
+            }
+        }
+    },
+
+    // 递归想订阅者发布
+    Publish: function (data) {
+        for(var y = 0, x = this._callbacks.length; y < x; y++) {
+            var callback = this._callbacks[y], l
+            callback.fn.apply(callback.context, data)
+            l = this._callbacks.length
+            if(l < x) {
+                y--
+                x = 1
+            }
+        }
+
+        for(var x in this._topcis) {
+            if(!this.stopped) {
+                if(this._topcis.hasOwnProperty(x)) {
+                    this._topcis[x].Publish(data);
+                }
+            }
+        }
+
+        this.stopped = false
+    }
+}
+
+function Mediator() {
+    if (!this instanceof Mediator) {
+        // 如果不存在mediator
+        return new Mediator()
+    } else {
+        // 存在赋予新的Topic数组列表，对订阅者进行管理
+        this._topcis = new Topic('')
+    }
+}
+
+// 订阅者的原型方法
+Mediator.prototype = {
+    GetTopic: function (namespace) {
+        var topic = this._topcis,
+            namespaceHierarchy = namespce.split(":")
+        if(namespace === '') {
+            return topic
+        }
+
+        if(namespaceHierarchy.length > 0) {
+            for(var i = 0, j = namespaceHierarchy.length; i<j;i++) {
+                if(!topic.hasTopic(namespaceHierarchy[i])) {
+                    topic.AddTopic(namespaceHierarchy)
+                }
+                topic = topic.ReturnTopic(namespaceHierarchy[i])
+            }
+        }
+        return topic
+    },
+    Subscribe: function (topicName, fn, options, context) {
+        var options = options || {},
+            context = context || {},
+            topic = this.GetTopic(topicName),
+            sub = topic.AddSubscriber(fn, options, context)
+
+        return sub
+    },
+    GetSubscriber: function (identifier, topic) {
+        return this.GetTopic(topic || '').GetSubscriber(identifier)
+    },
+    remove: function (topicName, identifier) {
+        this.GetTopic(topicName).RemoveSubscriber(identifier);
+    }
+}
+
--- Desiner/pub-sub.js
+++ Desiner/pub-sub.js
@@ -1,50 +1,159 @@
 // 发布订阅者模式
-var pubsub = {}
+var pubsub={} // 主题事件对象，连接发布者和订阅者
 
-(function (q) {
-    var topics = {},
-        subUid = -1
-        // 发布或广播事件，包含特定的topic名称和参数（比如传递的数据）
-        q.publish = function (topic, args) {
-            if (!topics[topic]) { // 如果不存在名为topic的事件
-                return false
-            }
+function start(q){
+    var topics = {}, // 主题事件的数组对象集合
+        subUid = -1 // 保存主题事件的id
 
-            var subscribers = topics[topic],
-                len = subscribers ? subscribers.length : 0
-
-            while(len--) {
-                subscribers[len].fun(topic, args)
-            }
-            return this
+    // 主题事件发布或者广播事件，其中包含topic的名称和参数。
+    q.publish = function (topic, args) {
+        // 如果该需要发布的事件不存在，则返回(在保存着所有主题事件的集合查找)
+        if (!topics[topic]) {
+            return false
         }
 
-        // 通过特定的名称和回调函数订阅事件，topic/event触发时执行事件
-        q.subscribe = function (topic, func) {
-            if (!topics[topic]) {
-                topics[topic] = []
-            }
+        // 该主题下的事件集合
+        var subscribers = topics[topic]
+            len = subscribers ? subscribers.length : 0
 
-            var token = (++subUid).toString()
-            topics[topic].push({
-                token: token,
-                func: func
-            })
-            return token
+        while(len--) {
+            // 发布了事件信息，就告知执行。
+            subscribers[len].func(topic, args)
+        }
+        // 调用后返回this
+        return this
+    }
+
+    // 此处就是订阅一个事件，topic/event触发时执行事件
+    q.subscribe = function (topic, func) {
+        //同样对订阅的事件进行调查， 是否存在，如果不存在，则创建一个新的，如果存在，就找到
+        if(!topics[topic]) {
+            topics[topic] = []
         }
 
-        // 基于订阅上的标记引用，通过特定topic取消订阅
-        q.unsubscribe = function (token) {
-            for (var m in topics) {
-                if (topics[m]) {
-                    for (var i = 0, j = topics[m].length; i < j; i++) {
-                        if (topics[m][i].token === token) {
-                            topics[m].splice(i, 1)
-                            return token
-                        }
+        // 为每一个事件保存一个token（唯一标识符）
+        var token = (++subUid).toString();
+
+        topics[topic].push({
+            token: token,
+            func: func
+        });
+        return token
+    }
+
+    // 移除某个特定的事件，传入他的token值
+    q.unsubscribe = function (token) {
+        for (var m in topics) {
+            if (topics[m]) {
+                for (var i = 0; i < topics[m].length; i++) {
+                    if (topics[m][i].token === token) {
+                        topics[m].splice(i, 1)
+                        return token
                     }
                 }
             }
-            return this
         }
-})(pubsub)
+        return this
+    }
+}// 将主题事件对象传入
+start(pubsub)
+// 实现上述的pubsub模式
+
+var messageLogger = function (topics, data) {
+    console.log('logging：' + topics + ':' + data)
+}
+
+var subscription = pubsub.subscribe('index/newMessage', messageLogger)
+var subscription2 = pubsub.subscribe('index/newMessage', function (topic, data){
+    if (data instanceof Array) {
+        console.log(data.reverse())
+    } else if(data instanceof Object){
+        console.log(Object.keys(data))
+    } else {
+        console.log('nothing to do')
+    }
+})
+
+pubsub.publish('index/newMessage', 'hello world!')
+
+pubsub.publish('index/newMessage', ['test', 'a', 'b', 'c'])
+
+pubsub.unsubscribe(subscription2)
+pubsub.publish('index/newMessage', {
+    sender: 'hello@google.com',
+    body: 'hey again'
+});
+
+pubsub.publish('index/newMessage', 'hello! are you still there?');
+
+var pubsub = {} // 创建一个中间对象，称为桥梁对象
+
+// 赋予桥梁对象通知发布者和订阅者的方法
+function setMethods(q) {
+    var topics = {}, // 保存着发布者的所有主题事件的集合
+        subUid = -1 // 用于保存每个主题的唯一标识符
+
+    // 发布一个主题事件
+    q.publish = function (topic, args) {
+        if (!topics[topic]) { // 如果存在该主题，则返回false
+            return false
+        }
+
+        var subscribers = topics[topic]
+            len = subscribers ? subscribers.length : 0
+        
+        while(len--) {
+            subscribers[len].func(topic, args)
+        }
+        return this
+    }
+
+    // 注册一个事件
+    q.subscribe = function (topic, func) {
+        // 如果不存在，则声明一个主题事件
+        if(!topics[topic]) {
+            topics[topic] = []
+        }
+
+        var token = (++subUid).toString()
+        topics[topic].push({
+            token: token,
+            func: func
+        })
+
+        return token
+    }
+
+    q.unsubscribe = function (token) {
+        for(var m in topics) {
+            if(topics[m]) {
+                for(var i = 0; i<topics.length; i++) {
+                    if(topics[m][i].token === token) {
+                        topics[m].splice(i, 1)
+                        return token
+                    }
+                }
+            }
+        }
+        return this
+    }
+}
+
+setMethods(pubsub)
+
+var ImPublish = {
+    publish: pubsub.publish
+}
+
+var ImSubscribe = {
+    subscribe: pubsub.subscribe
+}
+
+var GrayJay = ImSubscribe.subscribe('news', function (topic, data) {
+    console.log('GrayJay订阅了 ' + topic + ' 将不对内容进行处理')
+})
+
+var Miny = ImSubscribe.subscribe('news', function (topic, data) {
+    console.log('Miny订阅了 ' + topic + ' 接受发布者的内容：' + data)
+})
+ImPublish.publish('news', 'Today is a good day')
